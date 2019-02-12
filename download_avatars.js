d27cac76849eae48fa258334e465e8aa537d67e3
CClarkes <connorclarkes@gmail.com>
2019-02-12 02:08:38 +0000

    created loop for avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -7,15 +7,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-        'User-Agent': secrets;
+        'User-Agent': secrets
       }
     };
   
     request(options, function(err, res, body) {
       cb(err, body);
+      var data = JSON.parse(body);
+        if(err) {
+            console.log("Error, oh god oh h*ck" + error);
+        } else {
+            for(var i = 0; i < data.length; i++) {
+                var avatar = data[i];
+                console.log(avatar.avatar_url)
+            }
+        }
     });
   }
 getRepoContributors("jquery","jquery", function(err, result) {
-    console.log("Errors", err);
-    console.log("Result", result);
+    // console.log("Errors", err);
+    // console.log("Result", result);
 })
