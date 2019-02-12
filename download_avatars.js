4c58a51f5281b68fac036d61092cfc30c1f62ed7
CClarkes <connorclarkes@gmail.com>
2019-02-12 04:02:22 +0000

    final commit! All done!

--- download_avatars.js
+++ download_avatars.js
@@ -2,48 +2,47 @@ var request = require('request');
 var fs = require('fs');
 var secrets = require('./secrets');
 const https = require('https');
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 console.log("Welcome to the Github Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent': secrets
-      }
-    };
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+     headers: {
+      'User-Agent': secrets
+    }
+  };
   
-    request(options, function(err, res, body) {
-      cb(err, body);
-      const data = JSON.parse(body);
-        if(err) {
-            console.log("Error! oh god, oh h*ck" + error);
-        } else {
+  request(options, function(err, res, body) {
+    cb(err, body);
+    const data = JSON.parse(body);
+      if(err || res.statusCode != 200) {
+        console.log("oh g*d, oh h*ck, enter a valid Repo and Owner ya nerd!");
+          } else {
             for(var i = 0; i < data.length; i++) {
                 var avatar = data[i];
                 var images = avatar.avatar_url;
                 var username = avatar.login;
                 console.log(images);
-                downloadImageByURL(images, `./avatars/${username}` )
+              downloadImageByURL(images, `./avatars/${username}` )
             }
-        }
-        
-    });
+          }
+        });
     function downloadImageByURL(url, filePath) {
       https.get(url, function (response) {
           response.on('data', function(response) {
-              console.log("Downloading Image..." + "\n");
+            console.log("Downloading Image..." + "\n");
           })
             .pipe(fs.createWriteStream(filePath))
           response.on('end', function(end) {
               console.log("Download complete.");
           })
-
-      })
+        })
       }
-      
-  }
-getRepoContributors("jquery","jquery", function(err, result) {
+    }
+getRepoContributors(owner, repo, function(err, result) {
     // console.log("Errors", err);
     // console.log("Result", result);
 })
